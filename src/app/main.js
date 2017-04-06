060f24beb95c48486d7f19e69138ee92d8789c68
Jason Schindler <jason@types.codes>
2017-04-05 21:30:09 -0500

    Design improvements

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,6 @@
 require('@typopro/web-source-sans-pro/TypoPRO-SourceSansPro.css');
 require('@typopro/web-source-code-pro/TypoPRO-SourceCodePro.css');
+require('@typopro/web-coustard/TypoPRO-Coustard-Regular.css');
 require('font-awesome/css/font-awesome.css');
 
 require('./hvr_animations.css');
